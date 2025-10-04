3a7ff818432ac1d90e0823f1d2e5d6cafde0a01a
Giovanna Melo <gio.s.melo@gmail.com>
2025-10-03 23:21:38 -0300

    3

--- record6/src/main/java/com/aulas/Record6.java
+++ record6/src/main/java/com/aulas/Record6.java
@@ -4,6 +4,8 @@ public class Record6 {
 
     public static void main(String[] args) {
         Aluno a1 = new Aluno("giovanna", 3320, 10);
+        Aluno a2 = new Aluno("giovanna", 3320, 10);
+        //System.out.println(a1.equals(a2));
         a1.aprovado();
         System.out.println("nome: " + a1.nome());
         System.out.println("matricula: " + a1.matricula());
